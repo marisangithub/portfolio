ce4805c1764cde7287d37dac25b41a15dc14f9d4
marisangithub <marisan200320@gmail.com>
2025-09-11 17:06:34 +0530

    Home responsive

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,78 +3,72 @@ import profileImage from './assets/Portfolio-Profile.png';
 import Github from './assets/github.png';
 import LinkedIn from './assets/linkedin.png';
 import Whatsapp from './assets/whatsapp.png';
+import Footer from './Footer';
 import { Typewriter } from 'react-simple-typewriter';
-
-
 
 const Home = () => {
   return (
-    <>
-      <div className="container">
-        <div className="row mt-5">
-          <div className="col">
-            <div className="text align-items-center justify-content-end " >
-              <h2 className='font-heading'>Hello there, I am <br />  Marisan, {''} <span style={{ color: 'red' }}>
-                <Typewriter words={[ 'Full Stack Developer.', 'React Specialist.']} cursor cursorStyle='|' typeSpeed={70}
-                  deleteSpeed={50}
-                  delaySpeed={1500} loop /></span></h2>
-                  <div className='inlineflex'>
-                <a href="https://github.com/marisangithub">
-                  <img 
-                  src={Github} 
-                  alt="" 
-                  className='contactlogo mt-5'
+    <div className="page-wrapper">
+      {/* Page Content */}
+      <div className="page-content">
+        <div className="container mt-5">
+          <div className="row align-items-center">
+            
+            {/* LEFT COLUMN (Text) */}
+            <div className="col-12 col-md-6 text-center text-md-start mb-4 mb-md-0">
+              <h2 className="font-heading">
+                Hello there, I am <br /> Marisan,{' '}
+                <span style={{ color: 'red' }}>
+                  <Typewriter
+                    words={['Full Stack Developer.', 'React Specialist.']}
+                    cursor
+                    cursorStyle="|"
+                    typeSpeed={70}
+                    deleteSpeed={50}
+                    delaySpeed={1500}
+                    loop
                   />
+                </span>
+              </h2>
+
+              {/* Social icons */}
+              <div className="d-flex justify-content-center justify-content-md-start gap-3 mt-4">
+                <a href="https://github.com/marisangithub">
+                  <img src={Github} alt="GitHub" className="contactlogo" />
                 </a>
                 <a href="https://www.linkedin.com/in/mari-san-33bb8b288">
-                  <img 
-                  src={LinkedIn} 
-                  alt="" 
-                  className='contactlogo mt-5'
-                  />
+                  <img src={LinkedIn} alt="LinkedIn" className="contactlogo" />
                 </a>
                 <a href="https://wa.me/+916374536684">
-                  <img 
-                  src={Whatsapp} 
-                  alt="" 
-                  className='contactlogo mt-5'
-                  />
+                  <img src={Whatsapp} alt="WhatsApp" className="contactlogo" />
                 </a>
-                  </div>
-            </div>
-          </div>
-
-          <div className="col">
-            <div className="container d-flex justify-content-end align-item-center">
-              <div className="text-end">
-                <img
-                  src={profileImage}
-                  alt="marisan"
-                  className='glow-border img-fluid'
-                  style={{
-                    width: '300px',
-                    height: '300px',
-                    objectFit: 'cover',
-                    objectPosition: 'top',
-                    borderRadius:'50%',
-                    border:'2px solid red'
-                  }}
-                />
-
-
               </div>
+            </div>
+
+            {/* RIGHT COLUMN (Profile Image) */}
+            <div className="col-12 col-md-6 text-center">
+              <img
+                src={profileImage}
+                alt="marisan"
+                className="glow-border img-fluid"
+                style={{
+                  maxWidth: '300px',
+                  height: 'auto',
+                  borderRadius: '50%',
+                  border: '2px solid red',
+                  objectFit: 'cover',
+                  objectPosition: 'top',
+                }}
+              />
             </div>
           </div>
         </div>
       </div>
 
-<div className="footer-home">
-          <footer className="text-gray-400 text-center p-4 border-t border-gray-700">
-    © 2025 Marisan. All rights reserved.
-  </footer>
-</div>
-    </>
-  )
-}
+      {/* Sticky Footer */}
+      <Footer />
+    </div>
+  );
+};
 
 export default Home;
